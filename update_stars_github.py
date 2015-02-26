979d78e1801d7829177da2a0440cad1c9172f981
Erik de Jonge <erik@a8.nl>
2015-02-26 12:31:14 +0100

    markdown-to-ebook Thursday 26 February 2015 (week:4 day:57), 12:31:12

--- update_stars_github.py
+++ update_stars_github.py
@@ -7,8 +7,6 @@ import sys
 reload(sys)
 
 # noinspection PyUnresolvedReferences
-
-
 sys.setdefaultencoding("utf-8")
 
 import tarfile
@@ -21,7 +19,8 @@ from os.path import join, expanduser, exists, dirname
 from multiprocessing import Pool, cpu_count
 
 import shutil
-USERNAME = "erikdejonge"
+
+USERNAME = "<<username>>"
 
 
 def get_star_page(num):
@@ -29,6 +28,9 @@ def get_star_page(num):
     @type num: int
     @return: None
     """
+    if USERNAME == "<<username>>":
+        raise AssertionError("USERNAME: not set (line 23)")
+
     cmd = 'curl -s "https://api.github.com/users/' + USERNAME + '/starred?per_page=100&page=' + str(num) + '" > j' + str(num) + '.json'
     print cmd,
     os.system(cmd)
@@ -55,9 +57,9 @@ def clone_or_pull_from(remote):
         origin = r.remote()
         origin.fetch()
         origin.pull()
+
         #for change in origin.pull():
             #print "\033[90m", str(change).replace("origin/", "  "), "\033[0m"
-            
 
         ret = name + " " + str(r.active_branch) + " pulled"
         print "\033[37m", ret, "\033[0m"
@@ -156,14 +158,23 @@ def main():
 
                         if not exists(bupf):
                             os.mkdir(bupf)
+
                         bupf = join(bupf, "github")
+
                         if not exists(bupf):
                             os.mkdir(bupf)
+
                         tarname = join(bupf, pipes.quote(os.path.basename(delp)) + ".tar.gz")
                         tar = tarfile.open(tarname, "w:gz")
+
                         def modify(ti):
+                            """
+                            @type ti: str, unicode
+                            @return: None
+                            """
                             ti.name = ti.name.replace(dirname(delp).lstrip("/"), "")
                             return ti
+
                         tar.add(delp, filter=modify)
                         tar.close()
                         shutil.rmtree(delp)
@@ -174,9 +185,9 @@ def main():
 
     print "\033[32mDone\033[0m"
     if len(lt) != len(ltdir):
-
         dirnames = os.listdir(githubdir)
         showmessage = False
+
         for ghbn in names:
             found = False
 
@@ -189,20 +200,22 @@ def main():
                 print "\033[91m", ghbn, "\033[0m"
 
         for folder in dirnames:
-            found = False
+            if folder != "_newrepos":
+                found = False
 
-            for ghbn in names:
-                if ghbn == folder:
-                    found = True
-                    showmessage = True
+                for ghbn in names:
+                    if ghbn == folder:
+                        found = True
+                        showmessage = True
 
-            if not found:
-                print "\033[91m", folder, "\033[0m"
+                if not found:
+                    print "\033[91m", folder, "\033[0m"
 
         if showmessage:
             print "\033[31mItems and folderitems is not equal\033[0m"
             print "\033[90m", len(lt), "items github", "\033[0m"
             print "\033[90m", len(ltdir), "items folder", "\033[0m"
+
 
 if __name__ == "__main__":
     main()
