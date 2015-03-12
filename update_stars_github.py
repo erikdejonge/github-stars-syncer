eb8d4c8f02e132460da8ecce4b6a845452f9ebf6
Erik de Jonge <erik@a8.nl>
2015-03-12 13:33:53 +0100

    github Thursday 12 March 2015 (week:10 day:71), 13:33:53

--- update_stars_github.py
+++ update_stars_github.py
@@ -11,6 +11,7 @@ from future import standard_library
 standard_library.install_aliases()
 from builtins import str
 from builtins import range
+
 import sys
 import tarfile
 import json
@@ -64,13 +65,14 @@ def clone_or_pull_from(remote, name):
         origin = r.remote()
         origin.fetch()
         origin.pull()
-        ret = name + " " + str(r.active_branch) + " pulled"
-        #print "\033[37m", ret, "\033[0m"
+
+        # ret = name + " " + str(r.active_branch) + " pulled"
+        # print "\033[37m", ret, "\033[0m"
         sys.stdout.write("\033[37m.\033[0m")
         sys.stdout.flush()
     else:
-        ret = name + " " + str(Repo.clone_from(remote, gp).active_branch) + " cloned"
-        #print "\033[32m", ret, "\033[0m"
+        # ret = name + " " + str(Repo.clone_from(remote, gp).active_branch) + " cloned"
+        # print "\033[32m", ret, "\033[0m"
         newrepos = join(join(join(expanduser("~"), "workspace"), "github"), "_newrepos")
 
         if not exists(newrepos):
@@ -78,7 +80,7 @@ def clone_or_pull_from(remote, name):
 
         gp = join(newrepos, name)
         ret = name + " " + str(Repo.clone_from(remote, gp).active_branch) + " cloned"
-        print("\n\033[32m", ret, "\033[0m")
+        print("\033[32m", ret, "\033[0m")
 
     return True
 
@@ -122,121 +124,84 @@ def main():
     names = [x["name"] for x in lt]
     d = {}
     doubles_found = []
-    
+
     for n in names:
         if n in d:
             doubles_found.append(n)
 
         d[n] = True
 
-    ltdir = [x.strip().lower() for x in os.listdir(githubdir)]
     cnt = 0
     to_clone_or_pull = []
     ghbnames = []
-    doublecheckname = []
-    for i in lt:
-        if i["name"] not in doubles_found:
-            name = i["name"]
-            ghbnames.append(name)
-        else:
-            doubles_found.remove(i["name"])
-            
-            name = i["full_name"].replace("/", "_")
-            if name not in ltdir:
-                print("\033[95mdouble:", i["name"], "->", name, "\033[0m")
-            doublecheckname.append(name)
-            ghbnames.append(name)
 
+    for i in lt:
+        name = i["full_name"]
+        ghbnames.append(name)
         cnt += 1
         to_clone_or_pull.append((i["git_url"], name))
 
-    p = Pool(cpu_count() * 2)
-    debug = False
+    p = Pool(cpu_count() * 4)
 
-    if debug:
-        for arg, name in to_clone_or_pull:
-            start_clone_or_pull(arg)
-    else:
-        for retval in p.map(start_clone_or_pull, to_clone_or_pull):
-            if not retval:
-                print(retval)
+    # debug = False
+    # if debug:
+    #     for arg, name in to_clone_or_pull:
+    #         start_clone_or_pull(arg)
+    for retval in p.map(start_clone_or_pull, to_clone_or_pull):
+        if not retval:
+            print(retval)
 
-    for folder in os.listdir(githubdir):
-        found = False
-
-        for ghbn in ghbnames:
-            if ghbn == folder:
-                found = True
-
-        if not found:
-            delp = join(githubdir, folder)
-
-            if exists(delp):
-                if os.path.isdir(delp):
-                    if os.path.basename(delp) != "_newrepos":
-                        print("\n\033[31m", "backup and delete:", delp, "\033[0m")
-                        bupf = join(join(expanduser("~"), "workspace"), "backup")
-
-                        if not exists(bupf):
-                            os.mkdir(bupf)
-
-                        bupf = join(bupf, "github")
-
-                        if not exists(bupf):
-                            os.mkdir(bupf)
-
-                        tarname = join(bupf, pipes.quote(os.path.basename(delp)) + ".tar.gz")
-                        tar = tarfile.open(tarname, "w:gz")
-
-                        def modify(ti):
-                            """
-                            @type ti: str, unicode
-                            @return: None
-                            """
-                            ti.name = ti.name.replace(dirname(delp).lstrip("/"), "")
-                            return ti
-
-                        tar.add(delp, filter=modify)
-                        tar.close()
-                        shutil.rmtree(delp)
-                else:
-                    print("\033[91m", "WARNING: files in directory", delp, "\033[0m")
-            else:
-                print("\033[91m", delp, "\033[0m")
-
-    print("\n\033[32mDone\033[0m")
-    if len(lt) != len(ltdir):
-        dirnames = os.listdir(githubdir)
-        showmessage = False
-
-        for ghbn in names:
-            found = False
-
-            for folder in dirnames:
-                if ghbn == folder:
-                    found = True
-                    showmessage = True
-
-            if not found:
-                print("\033[31m", ghbn, "diff with gh\033[0m")
-
-        for folder in dirnames:
-            if folder != "_newrepos":
+    for motherf in os.listdir(githubdir):
+        if os.path.isdir(motherf):
+            for folder in os.listdir(join(githubdir, motherf)):
                 found = False
 
-                for ghbn in names:
-                    if ghbn == folder:
+                for ghbn in ghbnames:
+                    print(ghbn, join(motherf, folder))
+                    if ghbn == join(motherf, folder):
                         found = True
-                        showmessage = True
 
                 if not found:
-                    if folder not in doublecheckname:
-                        print("\033[34m", folder, "diff with dir\033[0m")
+                    delp = join(join(githubdir, motherf), folder)
+                    print(delp)
+                    if exists(delp):
+                        if os.path.isdir(delp):
+                            if os.path.basename(os.path.basename(delp)) != "_newrepos":
+                                print("\n\033[31m", "backup and delete:", delp, "\033[0m")
+                                bupf = join(join(expanduser("~"), "workspace"), "backup")
 
-        if showmessage:
-            #print "\033[31mItems and folderitems is not equal\033[0m"
-            print("\033[90m", len(lt), "items github", "\033[0m")
-            print("\033[90m", len(ltdir), "items folder", "\033[0m")
+                                if not exists(bupf):
+                                    os.mkdir(bupf)
+
+                                bupf = join(bupf, "github")
+
+                                if not exists(bupf):
+                                    os.mkdir(bupf)
+
+                                tarname = join(bupf, pipes.quote(os.path.basename(delp)) + ".tar.gz")
+                                tar = tarfile.open(tarname, "w:gz")
+
+                                def modify(ti):
+                                    """
+                                    @type ti: str, unicode
+                                    @return: None
+                                    """
+                                    ti.name = ti.name.replace(dirname(delp).lstrip("/"), "")
+                                    return ti
+
+                                tar.add(delp, filter=modify)
+                                tar.close()
+                                shutil.rmtree(delp)
+                        else:
+                            print("\033[91m", "WARNING: files in directory", delp, "\033[0m")
+                    else:
+                        print("\033[91m", delp, "\033[0m")
+
+    print("\n\033[32mDone\033[0m")
+    fp = open(join(githubdir, "list.txt"), "wt")
+
+    for i in lt:
+        fp.write(join(githubdir, i["full_name"]) + "\n")
 
 
 if __name__ == "__main__":
